2aaba8eb459bf636425d59a5b4849b539f180017
Abdul Dakkak <abduld@wolfram.com>
2017-03-17 23:11:33 +0000

    add wait function and split setting defaults for config

--- config.go
+++ config.go
@@ -15,20 +15,25 @@ type archiveConfig struct {
 	TempDir                 string              `json:"temp_dir" config"app.temp_dir" default:"default"`
 	CompressionFormatString string              `json:"compression_format" config:"archive.format" default:"gzip"`
 	CompressionFormat       archive.Compression `json:"-" config:"-"`
+	done                    chan struct{}       `json:"-" config:"-"`
 }
 
 var (
-	Config = &archiveConfig{}
+	Config = &archiveConfig{
+		done: make(chan struct{}),
+	}
 )
 
 func (archiveConfig) ConfigName() string {
 	return "Archive"
 }
 
-func (archiveConfig) SetDefaults() {
+func (a *archiveConfig) SetDefaults() {
+	vipertags.SetDefaults(a)
 }
 
 func (a *archiveConfig) Read() {
+	defer close(a.done)
 	vipertags.Fill(a)
 	if a.TempDir == "" || a.TempDir == "default" {
 		pth, err := ioutil.TempDir("", config.App.Name)
@@ -38,6 +43,10 @@ func (a *archiveConfig) Read() {
 		a.TempDir = pth
 	}
 	a.CompressionFormat = parseFormat(a.CompressionFormatString)
+}
+
+func (c archiveConfig) Wait() {
+	<-c.done
 }
 
 func (c archiveConfig) String() string {
